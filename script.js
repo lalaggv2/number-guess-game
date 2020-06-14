ad5301592b6eaa0e660e63ac3ad87c6d23240768
Lala <lalagg@hotmail.com>
2020-06-13 22:31:02 -0400

    finishing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;;
 
-const guesses = document.querySelector('.quesses');
+
+const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHigh = document.querySelector('.lowOrHigh');
 
@@ -9,6 +9,9 @@ const guessField = document.querySelector('.guessField');
 
 let guessCount = 1;
 let resetButton;
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;;
+console.log(randomNumber);
 
 function checkGuess() {
   let userGuess = Number(guessField.value);
@@ -19,11 +22,15 @@ function checkGuess() {
 
   if (userGuess === randomNumber) {
     lastResult.textContent = "Congratulations! You got it right!";
+    lastResult.style.backgroundColor = "green";
     lowOrHigh.textContent = '';
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = "GAME OVER!! SORRY!!";
     setGameOver();
   } else {
     lastResult.textContent = "Wrong!";
-    lastResult.getElementsByClassName.backgroundColor = 'red';
+    lastResult.style.backgroundColor = 'red';
     if (userGuess < randomNumber) {
       lowOrHigh.textContent = "Guess was too low!";
     } else if (userGuess > randomNumber) {
@@ -34,6 +41,34 @@ function checkGuess() {
   guessCount++;
   guessField.value = "";
   guessField.focus();
-
 };
 
+guessSubmit.addEventListener("click", checkGuess);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  document.body.append(resetButton);
+  resetButton.addEventListener('click', resetGame);
+};
+
+function resetGame() {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll('resultParas p');
+  for (let i = 0; i < resetParas.length; i++) {
+    resetParas[i].textContent = '';
+  }
+}
+
+resetButton.parentNode.removeChild(resetButton);
+
+guessField.disabled = false;
+guessSubmit.disabled = false;
+guessField.value = '';
+guessField.focus();
+
+lastResult.style.backgroundColor = 'white';
+
+randomNumber = Math.floor(Math.random() * 100) + 1;
