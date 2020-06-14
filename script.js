369cfcd44bd8b556ba971e6ac03d3403b2271407
Lala <lalagg@hotmail.com>
2020-06-13 21:49:29 -0400

    defining html and some functions

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ const guessField = document.querySelector('.guessField');
 let guessCount = 1;
 let resetButton;
 
+function checkGuess() {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = "Previous guesses: ";
+  }
+  guesses.textContent += userGuess + ' ';
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = "Congratulations! You got it right!";
+    lowOrHigh.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = "Wrong!";
+    lastResult.getElementsByClassName.backgroundColor = 'red';
+    if (userGuess < randomNumber) {
+      lowOrHigh.textContent = "Guess was too low!";
+    } else if (userGuess > randomNumber) {
+      lowOrHigh.textContent = "Guess was too high!";
+    }
+  }
+
+  guessCount++;
+  guessField.value = "";
+  guessField.focus();
+
+};
+
